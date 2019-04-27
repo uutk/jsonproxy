62ec736b18343585d78e1540adaa8fa5d0fa110e
bwat <kataria.rachit98@gmail.com>
2019-04-27 15:35:46 +0530

    Updated at 04/27/19 | 15-35-46 IST

--- print_proxy.py
+++ print_proxy.py
@@ -1,4 +1,8 @@
-import requests,json
-r=requests.get('http://sumanjay.ooo/jsonproxy/proxy.json')
-r_json=r.json()
-print(len(r_json['data']))
+#import requests,json
+from datetime import datetime
+now = datetime.datetime.now()
+time_now = (now.year, now.month, now.day, now.hour, now.minute, now.second)
+print(time_now)
+#r=requests.get('http://sumanjay.ooo/jsonproxy/proxy.json')
+#r_json=r.json()
+#print(len(r_json['data']))
--- script.py
+++ script.py
@@ -38,7 +38,7 @@ def push_to_git():
     india = timezone('Asia/Kolkata')
     in_time = datetime.now(india)
 	
-    commit_message= ("Updated at " + str(in_time.strftime('%D-%M-%Y | %H-%M-%S'))+" IST")
+    commit_message= ("Updated at " + str(in_time.strftime('%D | %H-%M-%S'))+" IST")
     #Stage the file
     call('git add .', shell = True)
     # Add your commit
