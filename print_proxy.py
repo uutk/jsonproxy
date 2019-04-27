8c3cd473fb0bd18294275bd7d7a6b52c5acd0139
bwat <kataria.rachit98@gmail.com>
2019-04-27 20:30:39 +0530

    Updated at 27-04-2019 | 20-30-39 IST

--- print_proxy.py
+++ print_proxy.py
@@ -1,4 +1,4 @@
 import requests,json
 r=requests.get('https://raw.githubusercontent.com/rachitkataria13/jsonproxy/master/proxy.json')
 r_json=r.json()
-print(len(r_json['data']))
+print(len(r_json['dat']))
