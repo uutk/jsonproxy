f452b09c332b54329b9cf68983a471cffe88795b
bwat <kataria.rachit98@gmail.com>
2019-04-27 15:36:32 +0530

    Updated at 04/27/19 | 15-36-31 IST

--- print_proxy.py
+++ print_proxy.py
@@ -1,8 +1,4 @@
-#import requests,json
-from datetime import datetime
-now = datetime.datetime.now()
-time_now = (now.year, now.month, now.day, now.hour, now.minute, now.second)
-print(time_now)
-#r=requests.get('http://sumanjay.ooo/jsonproxy/proxy.json')
-#r_json=r.json()
-#print(len(r_json['data']))
+import requests,json
+r=requests.get('http://sumanjay.ooo/jsonproxy/proxy.json')
+r_json=r.json()
+print(len(r_json['data']))
